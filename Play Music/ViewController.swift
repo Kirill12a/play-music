f0d9c2b8b9449b1afcb5ddb7483a3c948997c3bd
Kirill12a <kirilldrozd12@gmail.com>
2021-06-15 00:18:25 +0300

    увеличение +- громкости

--- Play Music/ViewController.swift
+++ Play Music/ViewController.swift
@@ -7,7 +7,7 @@
 import  AVFoundation
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
 
     @IBOutlet var buttin: UIButton!
     
@@ -15,13 +15,14 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        player?.numberOfLoops = 0
+        player?.delegate = self
         
     }
     
     @IBAction func didTapButton(){
         if let player = player, player.isPlaying{
             //stop
-//            buttin.setImage(UIImage(named: <#T##String#>), for: <#T##UIControl.State#>)
             buttin.setImage(UIImage(systemName: "stop.fill"), for: .normal)
             player.stop()
           
@@ -32,24 +33,50 @@ class ViewController: UIViewController {
             do {
                 try AVAudioSession.sharedInstance().setMode(.default)
                 try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
-                
                 guard let urlString = urlString else {
                     return
                 }
                 player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: urlString))
-                
                 guard let player = player else {
                     return
                 }
-                
                 player.play()
             }
-            
             catch {
                 print("что-то пошло не так")
             }
             
+            ///
+            audioPlayerDidFinishPlaying(player!, successfully: false)
+            ///
         }
     }
+    @IBAction func minVolume(_ sender: Any) {
+//        player?.volume = 0.1
+        for i in 0...10{
+            player?.volume -= 0.1
+        }
+    }
+    @IBAction func maxVolume(_ sender: Any) {
+//        player?.volume = 1
+        
+        for i in 1...10{
+            player?.volume += 0.1
+        }
+    }
+    
+    
+    
+    
+    ////
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        print("Stop")
+        player.play()
+    }
+    
+    
+    
 }
+
+
 
